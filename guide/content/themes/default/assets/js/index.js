da5299cdb4209bfdb752326c43184bec8362b40a
易航 <2136118039@qq.com>
2024-08-16 05:45:00 +0800

    update

--- guide/content/themes/default/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(() => {
-	$('.yqlj a').attr({
-		'target': '_blank'
-	})
-	$('#gb-ts').click(() => {
-		$('.tishi').fadeOut('slow')
-	})
-})
--- guide/content/themes/theme03/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!defined('ROOT')) exit;
-
-function themeConfig(system\admin\Form $form)
-{
-	$form->input('视频背景壁纸链接', 'background', 'https://s7.huoying666.com/video/20210527/f1abdbf33b1b07d96a0ba00640a5db9a/16221293575533c31498e6a09e.MP4_pre.mp4', 'url');
-}
